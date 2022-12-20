32fb16ab416356f37c7581fc99682b8e2a3320ea
EsmeeSchot06 <243750@edu.rocfriesepoort.nl>
2022-12-20 14:14:33 +0100

    Update database.php

--- database.php
+++ database.php
@@ -5,4 +5,9 @@ $dBUsername = "root";
 $dBPaswordname = "";
 $dBName = "uitleen_registratiesysteem";
 
-$conn = mysqli_connect($serverName, $dBUsername, $dBPaswordname, $dBName); 
+$conn = mysqli_connect($serverName, $dBUsername, $dBPaswordname, $dBName);
+
+if (!$conn) {
+    die("connection failed: " . mysqli_connect_error());
+}
+?>
