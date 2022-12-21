46d404ea384c5f51a17bda91e2e78cf2c677c693
EsmeeSchot06 <243750@edu.rocfriesepoort.nl>
2022-12-21 13:24:20 +0100

    Create database.php

---
+++ uitleensysteem/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$serverName = "localhost";
+$dBUsername = "root";
+$dBPaswordname = "";
+$dBName = "uitleen_registratiesysteem";
+
+$conn = mysqli_connect($serverName, $dBUsername, $dBPaswordname, $dBName);
+
+if (!$conn) {
+    die("connection failed: " . mysqli_connect_error());
+}
+?>
