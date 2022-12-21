61123fadf961d1cc56621dd865bb3ab5dbffc6c2
JoukeBoonstra <247567@edu.rocfriesepoort.nl>
2022-12-21 14:03:54 +0100

    Update Docenten pagina.php

--- Docenten pagina.php
+++ Docenten pagina.php
@@ -1,3 +1,20 @@
 <!DOCTYPE html>
 <html lang="nl">
+<head>
+    <style>
+    .flex-container {
+    display: flex;
+    flex-wrap: wrap;
+    }
+    </style>
+</head>
+<body>
+    <div class="flex-container">
+    <div>Uitlenen</div>
+    <div>Apparaten</div>
+    <br>
+    <div>Retourneren</div>
+    <div>Accounts</div>
+    </div>
+</body>
 </html>
