6ac1b7e124e58cff130de650078758d2edbb7e38
JoukeBoonstra <247567@edu.rocfriesepoort.nl>
2022-12-21 14:49:51 +0100

    Flex-countainer werkt maar het zijn nog geen knoppen

--- uitleensysteem/Docenten pagina.php
+++ uitleensysteem/Docenten pagina.php
@@ -4,15 +4,24 @@
     <style>
     .flex-container {
     display: flex;
-    flex-wrap: wrap;
+    flex-wrap: wrap-reverse;
+    }
+    .flex-container > div {
+    text-align: center;
+    width: 250px;
+    margin: 10px;
+    text-align: center;
+    line-height: 75px;
+    font-size: 30px;
     }
     </style>
 </head>
 <body>
     <div class="flex-container">
-    <div>Uitlenen</div>
-    <div>Apparaten</div>
-    <br>
+    <div><input type="button">Uitlenen</button></div>
+    <div><input type="button">Apparaten</button></div>
+    </div>
+    <div class="flex-container">
     <div>Retourneren</div>
     <div>Accounts</div>
     </div>
