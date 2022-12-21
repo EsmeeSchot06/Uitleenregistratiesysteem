a33a0862accc18ca03b81a30ae56f8f9661fe284
EsmeeSchot06 <243750@edu.rocfriesepoort.nl>
2022-12-21 15:19:07 +0100

    Update login.php

--- uitleensysteem/login.php
+++ uitleensysteem/login.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="nl">
-<head>
-
-</head>
-<body>
-   <div>
-    <form method="post">
-        <input type="hidden" name="from"value="login">
-        <input type="text" name="Email" placeholder="Email" required><br>
-        <input type="password" name="password" placeholder="Wachtwoord" required><br>
-		<button type="submit">Inloggen</button>
-    </form>
-   </div> 
-</body>
+  <head>
+    <link heref="">
+  </head>
+  <body>
+    <div>
+      <form method="post">
+          <input type="hidden" name="from"value="login">
+          <input type="text" name="Email" placeholder="Email" required><br>
+          <input type="password" name="password" placeholder="Wachtwoord" required><br>
+		      <button type="submit">Inloggen</button>
+      </form>
+    </div> 
+  </body>
 </html>
