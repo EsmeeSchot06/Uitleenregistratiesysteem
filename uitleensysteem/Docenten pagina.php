b1f2c4b123f33ad82ba15b695f1b97a9d3bba838
JoukeBoonstra <247567@edu.rocfriesepoort.nl>
2022-12-22 10:58:47 +0100

    knoppen staan nu meer naar midden

--- uitleensysteem/Docenten pagina.php
+++ uitleensysteem/Docenten pagina.php
@@ -5,25 +5,34 @@
     .flex-container {
     display: flex;
     flex-wrap: wrap-reverse;
+    margin-top: 100px;
+    margin-right: 100px;
+    margin-bottom: 100px;
+    margin-left: 250px;
+    text-align: center;
+    font-size: 30px;
     }
     .flex-container > div {
+    display: flex;
+    flex-wrap: wrap-reverse;
     text-align: center;
-    width: 250px;
-    margin: 10px;
+    margin-top: 100px;
+    margin-right: 100px;
+    margin-bottom: 100px;
+    margin-left: 250px;
     text-align: center;
-    line-height: 75px;
     font-size: 30px;
     }
     </style>
 </head>
 <body>
     <div class="flex-container">
-    <div><input type="button">Uitlenen</button></div>
-    <div><input type="button">Apparaten</button></div>
+    <div><button type="submit">Uitlenen</button></div>
+    <div><button type="submit">Apparaten</button></div>
     </div>
     <div class="flex-container">
-    <div>Retourneren</div>
-    <div>Accounts</div>
+    <div><button type="submit">Retourneren</button></div>
+    <div><button type="submit">Accounts</button></div>
     </div>
 </body>
 </html>
