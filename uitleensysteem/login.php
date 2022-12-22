6fe83839f89fbc300c3312f38cdbf1d70da285d5
EsmeeSchot06 <243750@edu.rocfriesepoort.nl>
2022-12-22 11:06:47 +0100

    Update login.php

--- uitleensysteem/login.php
+++ uitleensysteem/login.php
@@ -1,15 +1,61 @@
 <!DOCTYPE html>
 <html lang="nl">
   <head>
-    <link heref="">
+    <Style>
+      .Email{
+        position:absolute;
+            top: 20vh;
+            height: 4vh;
+            width: 20vw;
+            background-color: #fff;
+            color: #000;
+            border:5px solid black;
+            border-radius: 1vw;
+            text-decoration: none;
+            text-align: center;
+            margin-left: 40%;
+      }
+      .password{
+        position:absolute;
+            top: 30vh;
+            height: 4vh;
+            width: 20vw;
+            background-color: #fff;
+            color: #000;
+            border:5px solid black;
+            border-radius: 1vw;
+            text-decoration: none;
+            text-align: center;
+            margin-left: 40%;
+      }
+      .inloggen{
+        position:absolute;
+            top: 40vh;
+            height: 4vh;
+            width: 5vw;
+            background-color: #fff;
+            color: #000;
+            border:5px solid black;
+            border-radius: 1vw;
+            text-decoration: none;
+            text-align: center;
+            margin-left: 48%;
+    }
+    .inloggen:hover{
+      cursor: pointer;
+            background-color: #000;
+            color: #fff;
+            border-color: #000;
+    }
+    </Style>
   </head>
   <body>
     <div>
       <form method="post">
           <input type="hidden" name="from"value="login">
-          <input type="text" name="Email" placeholder="Email" required><br>
-          <input type="password" name="password" placeholder="Wachtwoord" required><br>
-		      <button type="submit">Inloggen</button>
+          <input class="Email" type="text" name="Email" placeholder="Email" required><br>
+          <input class="password" type="password" name="password" placeholder="Wachtwoord" required><br>
+		      <button class="inloggen" type="submit">Inloggen</button>
       </form>
     </div> 
   </body>
