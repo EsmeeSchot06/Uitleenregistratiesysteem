08af22782126abecd82f19a175e38dbcd72a4bfa
JoukeBoonstra <247567@edu.rocfriesepoort.nl>
2022-12-22 11:38:13 +0100

    mooie zoekbalk in midden
    dit is de mooie zoekbalk gemaakt met html en css voor de apparaten uitlenen pagina

---
+++ uitleensysteem/apparaten uitlenen.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+<style>
+input {
+    padding-right: 500px;
+    margin-left: 400px;
+}
+</style>
+</head>
+<body>
+    <div class="topnav">
+  <div class="search-container">
+      <input type="text" placeholder="Search." name="search">
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</div>
+</body>
+</html>
