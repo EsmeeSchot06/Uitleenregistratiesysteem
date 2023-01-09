dd2f288cdf1502edd7e8c8cdc94aaf0a8273bd77
JoukeBoonstra <247567@edu.rocfriesepoort.nl>
2023-01-09 15:03:55 +0100

    css toegevoegd

--- uitleensysteem/apparaten uitlenen.php
+++ uitleensysteem/apparaten uitlenen.php
@@ -6,6 +6,34 @@ input {
     padding-right: 500px;
     margin-left: 400px;
 }
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.price {
+  color: grey;
+  font-size: 22px;
+}
+
+.card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+.card button:hover {
+  opacity: 0.7;
+}
 </style>
 </head>
 <body>
@@ -16,5 +44,6 @@ input {
     </form>
   </div>
 </div>
+
 </body>
 </html>
