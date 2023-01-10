b404584997cd61d610b65e4728542631bf6daa99
JoukeBoonstra <247567@edu.rocfriesepoort.nl>
2023-01-10 13:30:38 +0100

    Update accounts toevoegen en verwijderen.php

--- uitleensysteem/accounts toevoegen en verwijderen.php
+++ uitleensysteem/accounts toevoegen en verwijderen.php
@@ -2,12 +2,17 @@
 <html>
 <head>
 <style>
-
-    </style>
+.knopje{
+    position:absolute;
+    margin-left:60%;
+}
+</style>
 </head>
 <body>
     <input type="email" name="email" placeholder="School e-mailadres docent" required></br></br>
     <input type="password" name="wachtwoord" placeholder="Wachtwoord" required></br></br>
+    <div class=" knopje">
     <button type="submit">Account aanmaken</button>
+    </div>
 </body>
 </html>
