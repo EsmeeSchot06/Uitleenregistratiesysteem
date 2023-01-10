cbe0b13649e8bff7a19594d7759c44811b940a4c
EsmeeSchot06 <243750@edu.rocfriesepoort.nl>
2023-01-10 13:35:47 +0100

    Update accounts toevoegen en verwijderen.php

--- uitleensysteem/accounts toevoegen en verwijderen.php
+++ uitleensysteem/accounts toevoegen en verwijderen.php
@@ -15,13 +15,17 @@
             text-align: center;
             margin-left:60%;
 }
+.knopje:hover{
+    cursor: pointer;
+            background-color: #000;
+            color: #fff;
+            border-color: #000;
+}
 </style>
 </head>
 <body>
     <input type="email" name="email" placeholder="School e-mailadres docent" required></br></br>
     <input type="password" name="wachtwoord" placeholder="Wachtwoord" required></br></br>
-    <div class=" knopje">
-    <button type="submit">Account aanmaken</button>
-    </div>
+    <button type="submit" class="knopje">Account aanmaken</button>
 </body>
 </html>
