3f5a5c7f576e50457e430ab7684c79cd9016dfcf
JoukeBoonstra <247567@edu.rocfriesepoort.nl>
2023-01-10 08:56:36 +0100

    Betere flexbox toegevoegd

--- uitleensysteem/apparaten uitlenen.php
+++ uitleensysteem/apparaten uitlenen.php
@@ -33,7 +33,15 @@ input {
 }
 .flex-container {
   display: flex;
-  flex-direction: column;
+  flex-direction: ;
+}
+.flex-container > div {
+  background-color: #f1f1f1;
+  width: 100px;
+  margin: 10px;
+  text-align: center;
+  line-height: 75px;
+  font-size: 30px;
 }
 </style>
 </head>
@@ -45,11 +53,15 @@ input {
     </form>
   </div>
 </div>
-<div class="card">
-  <div>
-<p>iPad</p>
-<div class="flex-container"></div> 
-</div>
+<div class="flex-container">
+  <div>Product 1</div>
+  <div>Product 2</div>
+  <div>Product 3</div>  
+  <div>Product 4</div>
+  <div>Product 5</div>
+  <div>Product 6</div>  
+  <div>Product 7</div>
+  <div>Product 8</div>
 </div>
 </body>
 </html>
