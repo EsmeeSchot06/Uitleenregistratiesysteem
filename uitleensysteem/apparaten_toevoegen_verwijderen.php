2bd39b41eb8c00a605e496ef489057672f8de8a0
EsmeeSchot06 <243750@edu.rocfriesepoort.nl>
2023-01-17 09:28:35 +0100

    wat dingen veranderd

--- uitleensysteem/apparaten_toevoegen_verwijderen.php
+++ uitleensysteem/apparaten_toevoegen_verwijderen.php
@@ -3,18 +3,49 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+<style>
+    .naam{
+        height: 6vh;
+            width: 20vw;
+            background-color: #fff;
+            color: #000;
+            border: 5px solid black;
+            border-radius: 1vw;
+            text-decoration: none;
+            text-align: center;
+    }
+    .merk{
+        height: 6vh;
+            width: 20vw;
+            background-color: #fff;
+            color: #000;
+            border: 5px solid black;
+            border-radius: 1vw;
+            text-decoration: none;
+            text-align: center;
+    }
+    .type{
+        height: 6vh;
+            width: 20vw;
+            background-color: #fff;
+            color: #000;
+            border: 5px solid black;
+            border-radius: 1vw;
+            text-decoration: none;
+            text-align: center;
+    }
+</style>
 </head>
     <body>
         <form method="POST" action="processapparatentoevoegen.php"> 
             naam:<br>
-            <input type="text" name="naam"> 
+            <input type="text" name="naam" class="naam"> 
             <br>
             merk:<br>
-            <input type="text" name="merk">
+            <input type="text" name="merk" class="merk">
             <br>
             type:<br>
-            <input type="text" name="type">
+            <input type="text" name="type"class="type">
             <br><br>
             <input type="submit" name="save" value="submit">
         </form> 
