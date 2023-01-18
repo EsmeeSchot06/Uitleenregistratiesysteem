61dc2ed40b9e007250216cde7ee20df0c1aae630
EsmeeSchot06 <243750@edu.rocfriesepoort.nl>
2023-01-18 13:20:05 +0100

    knopje naar docenten pagina toegevoegd

--- uitleensysteem/apparaten_uitlenen.php
+++ uitleensysteem/apparaten_uitlenen.php
@@ -64,9 +64,29 @@ input {
 .devices {
   text-align: center;
 }
+.knopje3{
+        position:absolute;
+            height: 6vh;
+            width: 6vw;
+            background-color: #fff;
+            color: #000;
+            border:5px solid black;
+            border-radius: 1vw;
+            text-decoration: none;
+            text-align: center;
+            margin-left:5%;  
+    }
+    .knopje3:hover{
+    cursor: pointer;
+            background-color: #000;
+            color: #fff;
+            border-color: #000;
+}
 </style>
 </head>
   <body>
+  <a href="Docenten_pagina.php"><button type="submit" class="knopje3">docenten pagina</button></a>
+    <br>
       <div class="flex-container">
       </div>
       <div class="devices">
--- uitleensysteem/overzicht_van_alle_apparaten.php
+++ uitleensysteem/overzicht_van_alle_apparaten.php
@@ -2,11 +2,29 @@
 <html>
 <head>
 <style>
-
+.knopje3{
+        position:absolute;
+            height: 6vh;
+            width: 6vw;
+            background-color: #fff;
+            color: #000;
+            border:5px solid black;
+            border-radius: 1vw;
+            text-decoration: none;
+            text-align: center;
+            margin-left:5%;  
+    }
+    .knopje3:hover{
+    cursor: pointer;
+            background-color: #000;
+            color: #fff;
+            border-color: #000;
+}
 </style>
-<title>Overzicht van alle apparaten</title>
 </head>
 <body>
+<a href="Docenten_pagina.php"><button type="submit" class="knopje3">docenten pagina</button></a>
+    <br>
 <input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search.." title="Type in a category">
 </body>
 </html>
