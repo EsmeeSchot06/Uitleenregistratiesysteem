6d894bb6d6c7a7bddbb2c085073996531f4b4d1f
EsmeeSchot06 <243750@edu.rocfriesepoort.nl>
2023-01-18 14:02:15 +0100

    wat veranderd

--- uitleensysteem/Docenten_pagina.php
+++ uitleensysteem/Docenten_pagina.php
@@ -2,12 +2,28 @@
 <html lang="nl">
 <head>
     <style>
+        .knopje4{
+        position:absolute;
+            background-color: #fff;
+            color: #000;
+            border:5px solid black;
+            border-radius: 1vw;
+            text-decoration: none;
+            text-align: center;
+            margin-left:2%;  
+    }
+    .knopje4:hover{
+    cursor: pointer;
+            background-color: #000;
+            color: #fff;
+            border-color: #000;
+    }
     .flex-container {
     display: flex;
     flex-wrap: wrap-reverse;
-    margin-top: 100px;
+    margin-top: 40px;
     margin-right: 100px;
-    margin-bottom: 100px;
+    margin-bottom: 60px;
     margin-left: 250px;
     text-align: center;
     font-size: 30px;
@@ -15,10 +31,9 @@
     .flex-container > div {
     display: flex;
     flex-wrap: wrap-reverse;
-    text-align: center;
-    margin-top: 50px;
+    margin-top: 40px;
     margin-right: 100px;
-    margin-bottom: 100px;
+    margin-bottom: 0px;
     margin-left: 120px;
     text-align: center;
     font-size: 30px;
@@ -26,10 +41,11 @@
     button {
     padding: 40px 80px 40px 80px;
     }
+    
     </style>
 </head>
 <body>
-    <p>klik <a href="index.php"> hier </a> om uit te loggen </p>
+    <a href="logout.php"><button type="submit" class="knopje4">uitloggen</button></a>
     <div class="flex-container">
     <div><a href="apparaten_uitlenen.php"><button type="submit">Apparaten uitlenen</button></a></div>
     <div><a href="overzicht_van_alle_apparaten.php"><button type="submit">Overzicht apparaten</button></a></div>
@@ -37,8 +53,6 @@
     <div class="flex-container">
     <div><a href="retourneer"><button type="submit"> retourneren</button></a></div>
     <div><a href="accounts_toevoegen_en_verwijderen.php"><button type="submit">Accounts beheren</button></a></div>
-    </div>
-    <div class="flex-container">
     <div><a href="apparaten_toevoegen_verwijderen.php"><button type="submit">Apparaten beheren</button></a></div>
     </div>
 </body>
