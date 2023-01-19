d3d771e43cfa7fabab661cf992fd4774e650e97a
JoukeBoonstra <247567@edu.rocfriesepoort.nl>
2023-01-19 15:14:14 +0100

    plaatje en tekst voor categorieën gemaakt

--- uitleensysteem/cameras.php
+++ uitleensysteem/cameras.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-
+    <title>Camera's</title>
 </head>
 <body>
-
+<img src="https://cdn.discordapp.com/attachments/1065628030187339876/1065631213404045322/hero_NikonZ400f4-5_96815-gk_nsp-43_article.png">
+<p>Dit zijn alle camera's</p>
 </body>
 </html>
--- uitleensysteem/snoeren.php
+++ uitleensysteem/snoeren.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-
+    <title>Snoeren</title>
 </head>
 <body>
-
+<img src="https://cdn.discordapp.com/attachments/1065628030187339876/1065629827744403476/550x358.png">
+<p>Dit zijn alle snoeren</p>
 </body>
 </html>
