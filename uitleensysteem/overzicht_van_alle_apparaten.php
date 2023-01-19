9ed5d425936e7c40c6a668c8bfec84d5819319b1
EsmeeSchot06 <243750@edu.rocfriesepoort.nl>
2023-01-19 13:55:27 +0100

    Update overzicht_van_alle_apparaten.php

--- uitleensysteem/overzicht_van_alle_apparaten.php
+++ uitleensysteem/overzicht_van_alle_apparaten.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+include ("database.php");
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -20,11 +24,23 @@
             color: #fff;
             border-color: #000;
 }
+#mySearch{
+    width: 100%;
+    font-size: 18px;
+    padding: 11px;
+    border: 2px solid black;
+}
 </style>
 </head>
 <body>
-<a href="Docenten_pagina.php"><button type="submit" class="knopje3">docenten pagina</button></a>
-    <br>
-<input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search.." title="Type in a category">
+    <input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search.." title="Type in a category">
+
+    <ul id="myMenu">
+        <li><a href="">ipad</a></li>
+        <li><a href="">laptops</a></li>
+        <li><a href="">snoeren</a></li>
+        <li><a href="">laders</a></li>
+        <li><a href="">cameras</a></li>
+    </ul>
 </body>
 </html>
