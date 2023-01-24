b803d8fbc2c7879e13a2aef6b51d1b6a28d29858
JoukeBoonstra <247567@edu.rocfriesepoort.nl>
2023-01-24 15:28:37 +0100

    Update retour.php

--- uitleensysteem/retour.php
+++ uitleensysteem/retour.php
@@ -1,15 +1,63 @@
 <?php
 session_start();
-include ("database.php");
+include("database.php");
+$sql = "SELECT id, naam, merk, type, uitgeleend FROM apparaten";
+$result = $con->query($sql);
 
+/*if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+  echo "id: " . $row["id"]. " - Name: " . $row["naam"]. " - merk: " . $row["merk"]. " - type: " . $row["type"]. " - uitgeleend: " . $row["uitgeleend"];
+}
+} else {
+  echo "Geen apparaten beschikbaar";
+}*/
 
+$sql = "UPDATE apparaten SET uitgeleend='uitgeleend' WHERE id= 2";
+if ($con->query($sql) === TRUE) {
+  echo "Record updated successfully";
+}else {
+  echo "Error updating record: " . $con->error;
+}
 ?>
 
 <!DOCTYPE html>
-<html>
-    <head>
-        <style>
-    .knopje3{
+<html lang="nl">
+<head>
+<style>
+input {
+    padding-right: 500px;
+    margin-left: 400px;
+}
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+.price {
+  color: grey;
+  font-size: 22px;
+}
+.card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+.card button:hover {
+  opacity: 0.7;
+}
+.devices {
+  text-align: center;
+}
+.knopje3{
         position:absolute;
             height: 6vh;
             width: 6vw;
@@ -29,7 +77,20 @@ include ("database.php");
 }
 </style>
 </head>
-<body>
-<a href="Docenten_pagina.php"><button type="submit" class="knopje3">docenten pagina</button></a>
-</body>
+  <body>
+  <a href="Docenten_pagina.php"><button type="submit" class="knopje3">docenten pagina</button></a>
+    <br>
+    <div class="devices">
+    <form method="POST">
+    <div class="updaten">
+    <input type="hidden" name="form" value="updaten">
+    </div>
+    </form>
+        <?php
+        if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) { ?>
+          <div><a><?= "Name: ".$row["naam"]." Merk: ".$row["merk"]." Type: ".$row["type"]. " - uitlenen: " . $row["uitgeleend"] ;?><a href="?uitlenen=<?= $row['id']?>">Uitlenen</a></a></div>
+        <?php }}else{?>Geen items gevonden<?php }?>
+      </div>
+  </body>
 </html>
