4d5b18322a69db66b1c721ae84c2f098846b1314
JoukeBoonstra <247567@edu.rocfriesepoort.nl>
2023-01-24 11:34:08 +0100

    kan nu ook weer terug naar docentenpagina

--- uitleensysteem/retour.php
+++ uitleensysteem/retour.php
@@ -5,9 +5,28 @@
 <!DOCTYPE html>
 <html>
     <head>
-
+        <style>
+    .knopje3{
+        position:absolute;
+            height: 6vh;
+            width: 6vw;
+            background-color: #fff;
+            color: #000;
+            border:5px solid black;
+            border-radius: 1vw;
+            text-decoration: none;
+            text-align: center;
+            margin-left:5%;  
+    }
+    .knopje3:hover{
+    cursor: pointer;
+            background-color: #000;
+            color: #fff;
+            border-color: #000;
+}
+</style>
 </head>
 <body>
-
+<a href="Docenten_pagina.php"><button type="submit" class="knopje3">docenten pagina</button></a>
 </body>
 </html>
