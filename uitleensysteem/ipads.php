5b7b9a88d1008a357d4a85a07d9f8dd1f08ff5d9
EsmeeSchot06 <243750@edu.rocfriesepoort.nl>
2023-02-01 14:27:56 +0100

    Update ipads.php

--- uitleensysteem/ipads.php
+++ uitleensysteem/ipads.php
@@ -18,7 +18,7 @@ $result = $con->query($sql);
     <?php
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            if ($row["categorieën"] === "ipads") { ?>
+            if ($row["categorieën"] === "ipad") { ?>
                 <a><?= "Name: " . $row["naam"] . " Merk: " . $row["merk"]; ?>
                     <img src="https://cdn.discordapp.com/attachments/1065628030187339876/1065628058758959114/9k.png">
                     <p>Dit zijn alle iPads</p>
